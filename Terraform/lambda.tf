2f40c2fcac3389f99d99a906c1c10ca048ee596b
carlos camacho <carlos0camacho@gmail.com>
2021-02-28 15:45:58 -0500

    update handler

--- Terraform/lambda.tf
+++ Terraform/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "func" {
   filename      = var.filename
   function_name = "audio_formater"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.handler"
+  handler       = "handler.mapAudio"
   runtime       = "nodejs12.x"
   publish = true
 
