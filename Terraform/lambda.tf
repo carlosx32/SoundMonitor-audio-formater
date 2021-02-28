1edb4a7162023bcbe39560a94985987bd1fe76a2
carlos camacho <carlos0camacho@gmail.com>
2021-02-28 14:06:09 -0500

    conf

--- Terraform/lambda.tf
+++ Terraform/lambda.tf
@@ -1,73 +1,13 @@
-
-# API Gateway
-resource "aws_api_gateway_rest_api" "api" {
-  name = "ApiEndpoint"
-}
-
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "resource"
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.api.id
-}
-
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda.invoke_arn
-}
-
-# Lambda
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  #source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_access_key_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
-  source_arn = "arn:aws:execute-api:us-east-1:827642192861:m431rrf715/*/GET/resource"
-}
-
-resource "aws_lambda_function" "lambda" {
-  filename      = "function.zip"
-  function_name = "sound-monitor-formater"
-  role          = aws_iam_role.role.arn
-  handler       = "lambda.lambda_handler"
+resource "aws_lambda_function" "func" {
+  filename      = var.filename
+  function_name = "soundmonitor-reporter"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.handler"
   runtime       = "nodejs12.x"
+  publish = true
 
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda.zip"))}"
-  source_code_hash = filebase64sha256("function.zip")
-}
-
-# IAM
-resource "aws_iam_role" "role" {
-  name = "myrole"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
+  depends_on = [
+    #aws_iam_role_policy_attachment.lambda_logs,
+    #aws_cloudwatch_log_group.soundmonitor-reporter
   ]
-}
-POLICY
 }
