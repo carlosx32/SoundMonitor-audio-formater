4a16a0218f845cd56cca9ba735cedb7aa5da46ec
carlos camacho <carlos0camacho@gmail.com>
2021-02-28 20:46:53 -0500

    update backend

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-monitor-provide-states-files"
+    bucket  = "terraform-states-files"
     key     = "s3event.tfstate"
     region  = "us-east-1"
   }
