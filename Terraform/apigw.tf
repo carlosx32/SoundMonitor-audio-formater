f2d2a5da8d8c0a8231b8c1df0e2603d3442ea43e
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 13:56:21 -0500

    http_method

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "audio_formater_api" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.audio_formater_api.id
   resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -20,6 +20,7 @@ resource "aws_api_gateway_integration" "audio_formater_integration" {
   rest_api_id             = aws_api_gateway_rest_api.audio_formater_api.id
   resource_id             = aws_api_gateway_rest_api.audio_formater_api.root_resource_id
   http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.audio_formater_lambda.invoke_arn
 }
