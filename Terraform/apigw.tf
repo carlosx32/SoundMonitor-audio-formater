36c82abae5e25228fc6636dd00a8aab0b7ae34e1
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 13:51:16 -0500

    resolve conflicts

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_method" "method" {
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  path_part   = "/formater"
+  path_part   = "formater"
   parent_id   = aws_api_gateway_rest_api.audio_formater_api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.audio_formater_api.id
 }
