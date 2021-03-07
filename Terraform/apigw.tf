bfba35ad8ac6e749a6545d2c0cf3e969b14e9cd0
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 15:54:28 -0500

    fix

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_rest_api" "apiLambda" {
 resource "aws_api_gateway_resource" "proxy" {
    rest_api_id = aws_api_gateway_rest_api.apiLambda.id
    parent_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
-   path_part   = "{proxy+}"
+   path_part   = "{+}"
 }
 
 resource "aws_api_gateway_method" "proxyMethod" {
@@ -20,6 +20,7 @@ resource "aws_api_gateway_integration" "lambda" {
    resource_id = aws_api_gateway_method.proxyMethod.resource_id
    http_method = aws_api_gateway_method.proxyMethod.http_method
 
+   path_part    = "formater"
    integration_http_method = "POST"
    type                    = "AWS_PROXY"
    uri                     = aws_lambda_function.audio_formater_lambda.invoke_arn
