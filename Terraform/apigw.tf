91f67dc4f192167daeaf08ee1d9fb41c8f1a6787
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 15:01:19 -0500

    new method

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -3,23 +3,26 @@ resource "aws_api_gateway_rest_api" "audio_formater_api" {
   description = "api for execute lambda"
 }
 
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "{formater+}
+  parent_id   = aws_api_gateway_rest_api.audio_formater_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.audio_formater_api.id
+}
+
+
+
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.audio_formater_api.id
   resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
+  http_method   = "ANY"
   authorization = "NONE"
-}
-
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "formater"
-  parent_id   = aws_api_gateway_rest_api.audio_formater_api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.audio_formater_api.id
 }
 
 resource "aws_api_gateway_integration" "audio_formater_integration" {
   rest_api_id             = aws_api_gateway_rest_api.audio_formater_api.id
   resource_id             = aws_api_gateway_rest_api.audio_formater_api.root_resource_id
   http_method             = aws_api_gateway_method.method.http_method
+
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.audio_formater_lambda.invoke_arn
@@ -32,4 +35,15 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = aws_lambda_function.audio_formater_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn = "arn:aws:execute-api:${var.aws_region}:${var.accountId}:${aws_api_gateway_rest_api.audio_formater_api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}
+
+
+resource "aws_api_gateway_deployment" "example" {
+   depends_on = [
+     aws_api_gateway_integration.apigw_lambda,
+     aws_api_gateway_integration.lambda_root,
+   ]
+
+   rest_api_id = aws_api_gateway_rest_api.example.id
+   stage_name  = "test"
 }
