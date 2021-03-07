cdd8f525795d5a6e630012fc61b9bba2c3936f8b
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 16:03:49 -0500

    test

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -31,7 +31,7 @@ resource "aws_api_gateway_integration" "lambda" {
 resource "aws_api_gateway_method" "proxy_root" {
     rest_api_id   = aws_api_gateway_rest_api.apiLambda.id
     resource_id   = aws_api_gateway_rest_api.apiLambda.root_resource_id
-    http_method   = "POST"
+    http_method   = "NONE"
     authorization = "NONE"
 }
 
