9473c435ba7e1dccce6aa40d14954490b5c8da20
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 15:39:27 -0500

    oas

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -103,10 +103,18 @@ resource "aws_api_gateway_rest_api" "audio_formater_api" {
 #}
 
 resource "aws_api_gateway_deployment" "audio_formater_deploy" {
-  depends_on = [
-    aws_api_gateway_rest_api.audio_formater_api,
-  ]
-
   rest_api_id = aws_api_gateway_rest_api.audio_formater_api.id
-  stage_name  = "prod"
+   triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.audio_formater_api.body))
+  }
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  stage_name    = "prod"
 }
