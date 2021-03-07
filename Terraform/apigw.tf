ae0cf0f51cb62a2f4a72174bbbc2710d5688ad3f
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 15:14:56 -0500

    fix

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -4,7 +4,7 @@ resource "aws_api_gateway_rest_api" "audio_formater_api" {
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  path_part   = "{formater+}"
+  path_part   = "formater"
   parent_id   = aws_api_gateway_rest_api.audio_formater_api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.audio_formater_api.id
 }
