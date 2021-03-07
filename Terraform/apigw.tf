381eaa89827ac42d39de6197b77c19bee3bdd805
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 14:52:52 -0500

    test

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "audio_formater_api" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.audio_formater_api.id
   resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "ANY"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
