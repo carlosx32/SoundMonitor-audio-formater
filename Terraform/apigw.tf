718ee39b81cd3df0e140ac9280cf390c5744321a
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 13:41:48 -0500

    var deleted

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -3,6 +3,12 @@ resource "aws_api_gateway_rest_api" "audio_formater_api" {
   description = "api for execute lambda"
 }
 
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.audio_formater_api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
 
 resource "aws_api_gateway_resource" "resource" {
   path_part   = "/formater"
