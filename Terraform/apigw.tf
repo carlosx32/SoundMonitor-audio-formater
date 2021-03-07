2cc397098de9b0a16332251ad5851103466dffaa
carlos camacho <carlos0camacho@gmail.com>
2021-03-07 15:44:16 -0500

    test

--- Terraform/apigw.tf
+++ Terraform/apigw.tf
@@ -17,7 +17,7 @@ resource "aws_api_gateway_rest_api" "audio_formater_api" {
       }
     ],
     "paths" : {
-      "/formater" : {
+      "/formater2" : {
         "post" : {
           "responses" : {
             "200" : {
